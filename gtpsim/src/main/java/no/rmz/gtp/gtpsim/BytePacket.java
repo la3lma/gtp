aa947a1500e992b6a6ddcc0ea9d4ec5387389267
Bjørn Remseth <rmz@comoyo.com>
2016-09-12 20:23:38 +0200

    Working towards confidence on bit shuffling.

--- gtpsim/src/main/java/no/rmz/gtp/gtpsim/BytePacket.java
+++ gtpsim/src/main/java/no/rmz/gtp/gtpsim/BytePacket.java
@@ -1,6 +1,7 @@
 package no.rmz.gtp.gtpsim;
 
 import com.google.common.base.Preconditions;
+import static com.google.common.base.Preconditions.checkArgument;
 
 public final class BytePacket {
 
@@ -10,21 +11,61 @@ public final class BytePacket {
         this.packet = Preconditions.checkNotNull(packet);
     }
 
-    public int getBits(final int offset, final int mask) {
-        return getBits(offset, (byte) mask);
+    private byte getMask(final int offset, final int length) {
+        checkLengthAndOffset(length, offset);
+
+        byte result = 0b1;
+        for (int i = 0; i < length - 1; i++) {
+            result <<= 1;
+            result |= 0b1;
+        }
+        result <<= offset;
+        return result;
     }
 
-    public int getBits(final int offset, final byte mask) {
+    private void checkLengthAndOffset(final int length, final int offset) {
+        checkArgument(length > 0);
+        checkArgument(length <= 7);
+        checkArgument(offset >= 0);
+        checkArgument(offset <= 7);
+    }
+
+    /**
+     * Getting bits off the first byte of the array, interpreting them as an
+     * unsigned binary number.
+     *
+     * @param offset
+     * @param length
+     * @return
+     */
+    public int getBits(
+            final int offset,
+            final int length) {
+        final byte mask = getMask(offset, length);
         final byte unmasked = packet[0];
-        final byte masked = (byte) (unmasked & mask);
-        final byte signedResult = (byte) (masked >> offset);
-        final byte unsignedResult = (byte) ((byte) 0xff & signedResult);
-        return unsignedResult;
+        byte masked = (byte) (unmasked & mask);
+        final byte rawShifted = (byte) (masked >> offset);
+        final byte shifted = (byte) ((byte) 0xff & rawShifted);
+        return shifted;
     }
 
-    public void setBits(final int value, final int offset, final byte mask) {
-        final byte bv = (byte) value;
-        final byte storedBits = (byte) (bv << offset);
-        packet[0] = (byte) (storedBits | (byte) (packet[0] & mask));
+    /**
+     * Setting bits in the first byte of the array.
+     *
+     * @param offset
+     * @param length
+     * @param value
+     */
+    public void setBits(
+            final int offset,
+            final int length,
+            final int value) {
+        checkLengthAndOffset(length, offset);
+
+        final byte mask = getMask(offset, length);
+        final byte bv = packet[0];
+        final byte storedBits = (byte) ((value << offset) | (bv & ~mask));
+
+        packet[0] = storedBits;
     }
 }
--- gtpsim/src/test/java/no/rmz/gtp/gtpsim/BytePacketTest.java
+++ gtpsim/src/test/java/no/rmz/gtp/gtpsim/BytePacketTest.java
@@ -3,12 +3,11 @@ package no.rmz.gtp.gtpsim;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-// XXX Signs are messing things up!
 public class BytePacketTest {
 
     private final static byte THREE_SET_SHIFT_2 = 0b00011100;
-    private final static byte RIGHTMOST_SET = 0b00000001;
-    private final static byte LEFTMOST_SET =  (byte) (0xff & (0b1 << 7));
+    private final static byte RIGHTMOST_SET = 0b1;
+    private final static byte LEFTMOST_SET = (byte)  (0b1 << 6);
 
     @Test
     public void getRightmostBit() {
@@ -20,16 +19,46 @@ public class BytePacketTest {
     @Test
     public void getLeftmostBit() {
         final BytePacket bp = new BytePacket(new byte[]{LEFTMOST_SET});
-        assertEquals(1, bp.getBits(7, LEFTMOST_SET));
+        assertEquals(1, bp.getBits(6, 1));
     }
 
     @Test
     public void getThreeBitsShiftedtwoLeft() {
         final BytePacket bp
                 = new BytePacket(new byte[]{THREE_SET_SHIFT_2});
-
         // XXX So this is screwed up.
-        assertEquals(0b111, bp.getBits(2, THREE_SET_SHIFT_2));
+        assertEquals(0b111, bp.getBits(2, 3));
     }
 
+    @Test
+    public void getSingleBitShiftedRound() {
+        for (int i = 0; i < 7; i++) {
+            final byte b = (byte) (0b1 << i);
+            final BytePacket bp
+                    = new BytePacket(new byte[]{b});
+            assertOnlyBitISet(i, bp);
+        }
+    }
+
+    @Test
+    public void setSingleBitShiftedRound() {
+        for (int i = 0; i < 7; i++) {
+            final BytePacket bp
+                    = new BytePacket(new byte[]{0});
+            bp.setBits(i, 1, 1);
+            assertOnlyBitISet(i, bp);
+        }
+    }
+
+    private void assertOnlyBitISet(int i, final BytePacket bp) {
+        for (int j = 0; j < 7; j++) {
+            final String msg
+                    = String.format("j = %d, i = %d", j, i);
+            if (i == j) {
+                assertEquals("expect 1: " + msg, 0b1, bp.getBits(i, 1));
+            } else {
+                assertEquals("expect 0: " + msg, 0, bp.getBits(i, 1));
+            }
+        }
+    }
 }
